9f966f2bace1aa28548d16839732cbb4c0076fa3
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2023-12-19 12:15:48 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ const App = () =>{
       const updateTodos = [...todos];
       //복사된 배열에서 지정된 자리값(index)를 한개 제거하겠다는 의미
       //updateTodos = 복사된 배열
-      //slice : 제거 
-      updateTodos.slice(index,1);
+      //slice : 배열에서 일부분 복사해서 새로운 배열 반환, 원본에 있던 배열은 변경되지않지만 시작-종료 인덱스
+      //splice: 둘다 배열을 조작하는데 사용 / 배열의 내용을 수정하거나 삭제하고 필요에따라 새로운 요소를 추가할 수 있음
+      updateTodos.splice(index,1);
       //내가 제거하고싶은 할 일을 제거한 후 setTodos를 활용해서 할일 목록을 재설정
       setTodos(updateTodos);
       
